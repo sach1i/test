e2ae036b5839c759a5506220a2421ca42c914d3e
BogdanNovikov <bohdan.novikov94@gmail.com>
2020-12-25 18:36:07 +0100

    Update new.py

--- new.py
+++ new.py
@@ -6,16 +6,10 @@ class MainApp(App):
     def my_func(self):
         gps.configure(on_location=self.on_gps)
         gps.start()
-        gps.stop()
 
     def on_gps(self, **kwargs):
-        lat = 'lat: {lat}'.format(**kwargs)
-        lon = 'lon: {lon}'.format(**kwargs)
-        return (lat,lon)
+        gps.stop()
+        return Button(text=kwargs)
 
-    def build(self):
-        self.my_func()
-        result = self.on_gps()
-        return Button(text=result)
 
 MainApp().run()
